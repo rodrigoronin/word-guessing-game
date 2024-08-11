267418bcf42d04d419fbb6f4252dae1fff436acd
rodrigo ronin <liramailbox@gmail.com>
2024-08-11 12:15:09 -0300

    feat: chunk array && word validation
    Validate words and paint letters (no duplicated validation yet)

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,23 @@ const chars: HTMLElement[] = [...nodeList];
 let currentIndex: number = 0;
 let wordBuffer: string = '';
 let wordSize: number = 5;
+let letterBoxIndex: number = 0;
+
+// break the nodeslist into smaller arrays
+const chunkArray = <T>(array: T[], size: number): T[][] => {
+  const result: T[][] = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+
+  return result;
+}
+
+const letterBoxes: HTMLElement[][] = chunkArray(chars, 5);
 
 const getWord = async (): Promise<Word> => {
-  const wordFetch: Response = await fetch('https://words.dev-apis.com/word-of-the-day');
+  const wordFetch: Response = await fetch('https://words.dev-apis.com/word-of-the-day?puzzle=10');
   const word: Word = await wordFetch.json();
 
   return word;
@@ -40,10 +54,9 @@ const writeChar = (input: KeyboardEvent): void => {
   if (input.key.length === 1 && regex.test(input.key)) {
     for (let i = 0; i < chars.length; i++) {
       if (!chars[i].textContent && wordBuffer.length < wordSize) {
-        chars[i].textContent = input.key;
+        chars[i].textContent = input.key.toUpperCase();
         wordBuffer += input.key;
         currentIndex = i;
-        console.log(currentIndex);
 
         break;
       }
@@ -55,8 +68,30 @@ const validateWord = () => {
   if (wordBuffer === word.word) {
     console.log('You win!');
   }
-  console.log(wordBuffer);
+
+  for (let i = 0; i < word.word.length; i++) {
+    const letter = word.word[i];
+
+    for (let j = 0; j < wordBuffer.length; j++) {
+      const guess = wordBuffer[j];
+
+      if (word.word.indexOf(guess) === -1) {
+        console.log(guess, ' absent');
+      }
+
+      if (guess === letter && i === j) {
+        letterBoxes[letterBoxIndex][j].style.backgroundColor = 'green';
+        console.log(guess, ' correct');
+      }
+
+      if (guess === letter && i !== j) {
+        console.log(guess, ' misplaced');
+      }
+    }
+  }
+
   wordBuffer = '';
+  letterBoxIndex++;
 }
 
 const eraseChar = (): void => {
@@ -66,7 +101,6 @@ const eraseChar = (): void => {
 
     chars[currentIndex].textContent = '';
     currentIndex--;
-    console.log(wordBuffer);
   }
 }
 
